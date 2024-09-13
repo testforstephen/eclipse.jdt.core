574d28b3eafba1af00a914495ff7157613a9290d
Rob Stryker <stryker@redhat.com>
2024-09-13 01:45:00 -0400

    Make all model test suites recursively filterable
    Signed-off-by: Rob Stryker <stryker@redhat.com>
    
    Remove unused import
    
    Signed-off-by: Rob Stryker <stryker@redhat.com>

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/RunVariousSwitchTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/RunVariousSwitchTests.java
@@ -23,6 +23,7 @@ import org.eclipse.jdt.core.tests.compiler.parser.ComplianceDiagnoseTest;
 import org.eclipse.jdt.core.tests.compiler.regression.*;
 import org.eclipse.jdt.core.tests.dom.ConverterTestSetup;
 import org.eclipse.jdt.core.tests.model.JavaSearchBugs14SwitchExpressionTests;
+import org.eclipse.jdt.core.tests.model.RecursivelyFilterableTestSuite;
 import org.eclipse.jdt.core.tests.rewrite.describing.ASTRewritingSwitchExpressionsTest;
 import org.eclipse.jdt.core.tests.rewrite.describing.ASTRewritingSwitchPatternTest;
 import org.eclipse.jdt.core.tests.util.AbstractCompilerTest;
@@ -57,7 +58,7 @@ public class RunVariousSwitchTests extends TestCase {
 	}
 
 	public static Test suite() {
-		TestSuite ts = new TestSuite(RunVariousSwitchTests.class.getName());
+		TestSuite ts = new RecursivelyFilterableTestSuite(RunVariousSwitchTests.class.getName());
 
 		Class[] testClasses = getAllTestClasses();
 		addTestsToSuite(ts, testClasses);
--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterJavadocTest.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterJavadocTest.java
@@ -33,6 +33,8 @@ import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.tests.javac.JavacTestIgnore;
 import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 import org.eclipse.jdt.internal.compiler.parser.ScannerHelper;
+import org.junit.Ignore;
+import org.junit.experimental.categories.Category;
 
 /**
  * Class to test DOM/AST nodes built for Javadoc comments.
@@ -1914,7 +1916,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	/**
 	 * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=53075"
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
 	public void testBug53075() throws JavaModelException {
 		ICompilationUnit unit = getCompilationUnit("Converter" , "src", "javadoc.testBug53075", "X.java"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		boolean pb = this.packageBinding;
@@ -1966,7 +1968,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 		this.stopOnFailure = true;
 	}
 
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_BEHAVIOR_STRANGE)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_BEHAVIOR_STRANGE)
 	public void testBug54424() throws JavaModelException {
 		this.stopOnFailure = false;
 		String [] unbound = { "tho",
@@ -2001,7 +2003,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	/**
 	 * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=51660"
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.TESTS_SPECIFIC_RESULT_FOR_UNDEFINED_BEHAVIOR)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.TESTS_SPECIFIC_RESULT_FOR_UNDEFINED_BEHAVIOR)
 	public void testBug51660() throws JavaModelException {
 		this.stopOnFailure = false;
 		ICompilationUnit unit = getCompilationUnit("Converter" , "src", "javadoc.testBug51660", "Test.java"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
@@ -2094,7 +2096,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * Bug 65174: Spurious "Javadoc: Missing reference" error
 	 * @see "http://bugs.eclipse.org/bugs/show_bug.cgi?id=65174"
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
 	public void testBug65174() throws JavaModelException {
 		verifyComments("testBug65174");
 	}
@@ -2103,7 +2105,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * Bug 65253: [Javadoc] @@tag is wrongly parsed as @tag
 	 * @see "http://bugs.eclipse.org/bugs/show_bug.cgi?id=65253"
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_VIOLATES_SPEC)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_VIOLATES_SPEC)
 	// See https://docs.oracle.com/en/java/javase/22/docs/specs/javadoc/doc-comment-spec.html
 	//@@, to represent @, to prevent it from being interpreted as part of the introduction of a block or inline tag,
 	public void testBug65253() throws JavaModelException {
@@ -2171,14 +2173,14 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * @see "http://bugs.eclipse.org/bugs/show_bug.cgi?id=70892"
 	 * @deprecated using deprecated code
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
 	public void testBug70892_JLS2() throws JavaModelException {
 		int level = this.astLevel;
 		this.astLevel = AST.JLS2;
 		verifyComments("testBug70892");
 		this.astLevel = level;
 	}
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
 	public void testBug70892_JLS3() throws JavaModelException {
 		int level = this.astLevel;
 		this.astLevel = getJLS3();
@@ -2278,7 +2280,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=80221"
 	 */
 	// Resolving "Object" should not be controversial since it is a well known type
-	@JavacTestIgnore(cause=JavacTestIgnore.VALID_ALTERNATIVE_IMPL)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.VALID_ALTERNATIVE_IMPL)
 	public void testBug80221() throws JavaModelException {
 		this.workingCopies = new ICompilationUnit[1];
 		this.astLevel = getJLS3();
@@ -2535,7 +2537,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 		}
 	}
 
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_VIOLATES_SPEC)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_VIOLATES_SPEC)
 	public void testBug93880_15c() throws JavaModelException {
 		this.workingCopies = new ICompilationUnit[1];
 		this.astLevel = getJLS3();
@@ -2753,7 +2755,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * Bug 99507: [javadoc] Infinit loop in DocCommentParser
 	 * @see "http://bugs.eclipse.org/bugs/show_bug.cgi?id=99507"
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_VIOLATES_SPEC)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_VIOLATES_SPEC)
 	public void testBug99507() throws JavaModelException {
 		this.workingCopies = new ICompilationUnit[1];
 		this.workingCopies[0] = getWorkingCopy("/Converter15/src/javadoc/b99507/X.java",
@@ -2940,7 +2942,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	// Syntax like @See I.VE#I.VE(params) is not allowed by javac, specifically
 	// the dot in the method name is not allowed and causes a DCErroneous
 	// See https://docs.oracle.com/javase/7/docs/technotes/tools/windows/javadoc.html#see
-	@JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.JDT_RECOVERS_FROM_BAD_INPUTS)
 	public void testBug103304() throws JavaModelException {
 		this.packageBinding = false; // do NOT verify that qualification only can be package name
 		this.workingCopies = new ICompilationUnit[1];
@@ -3250,7 +3252,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * bug125903: [javadoc] Treat whitespace in javadoc tags as invalid tags
 	 * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=125903"
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.TESTS_SPECIFIC_RESULT_FOR_UNDEFINED_BEHAVIOR)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.TESTS_SPECIFIC_RESULT_FOR_UNDEFINED_BEHAVIOR)
 	public void testBug125903() throws JavaModelException {
 		this.workingCopies = new ICompilationUnit[1];
 		this.astLevel = getJLS3();
@@ -3408,7 +3410,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 		verifyComments(unit);
 	}
 	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=196714
-	@JavacTestIgnore(cause=JavacTestIgnore.VALID_ALTERNATIVE_IMPL)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.VALID_ALTERNATIVE_IMPL)
 	public void test109() throws JavaModelException {
 		verifyComments("test109");
 	}
@@ -3496,7 +3498,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=206345"
 	 * @deprecated
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.TESTS_SPECIFIC_RESULT_FOR_UNDEFINED_BEHAVIOR)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.TESTS_SPECIFIC_RESULT_FOR_UNDEFINED_BEHAVIOR)
 	public void testBug206345a() throws JavaModelException {
 		this.workingCopies = new ICompilationUnit[1];
 		this.astLevel = AST.JLS3;
@@ -3544,7 +3546,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 *
 	 * @deprecated
 	 */
-	@JavacTestIgnore(cause=JavacTestIgnore.TESTS_SPECIFIC_RESULT_FOR_UNDEFINED_BEHAVIOR)
+	@Category(value=Ignore.class) @JavacTestIgnore(cause=JavacTestIgnore.TESTS_SPECIFIC_RESULT_FOR_UNDEFINED_BEHAVIOR)
 	public void testBug206345b() throws JavaModelException {
 		this.workingCopies = new ICompilationUnit[1];
 		this.astLevel = AST.JLS3;
