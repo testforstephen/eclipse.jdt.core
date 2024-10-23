5cc014d6d99488d68bd50fdf22faef52764751e0
Mickael Istria <mistria@redhat.com>
2024-10-23 21:53:19 +0200

    Avoid NumberFormatException when trying to get latest JRE.

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/CompilationUnit.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/CompilationUnit.java
@@ -481,8 +481,9 @@ public org.eclipse.jdt.core.dom.CompilationUnit getOrBuildAST(WorkingCopyOwner w
 		return this.ast;
 	}
 	Map<String, String> options = getOptions(true);
-	int jlsLevel = Integer.parseInt(options.getOrDefault(JavaCore.COMPILER_SOURCE, Integer.toString(AST.getJLSLatest())));
-	ASTParser parser = ASTParser.newParser(jlsLevel);
+	ASTParser parser = ASTParser.newParser(new AST(options).apiLevel()); // go through AST constructor to convert options to apiLevel
+	// but we should probably instead just use the latest Java version
+	// supported by the compiler
 	parser.setWorkingCopyOwner(workingCopyOwner);
 	parser.setSource(this);
 	// greedily enable everything assuming the AST will be used extensively for edition
